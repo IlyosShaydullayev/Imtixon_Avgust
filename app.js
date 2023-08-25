b185869faa6cdf2dd52948a0518af464adc55cc6
Ilyos <ilyosshaydullaye212@gmail.com>
2023-08-25 12:36:31 +0500

    End

---
+++ app.js
@@ -0,0 +1,49 @@
+//1) Scopelar 3ga bo'linadi:
+//Global - let a = 'Hello'; console.log(a);
+//Function - funksiya
+//Block - if for while do while switch
+//2)
+// function num(){
+//     var n = 6;
+//     for (let i=1; i<n; i++){
+//         if(n%i==0){
+//             console.log(i);
+//         }
+//         // return count;
+//     }
+// }
+// console.log(num());
+
+//3)
+// function num(n){
+//     switch(n){
+//         case 1:
+//             console.log("yomon");
+//             break;
+//         case 2:
+//             console.log("qoniqarsiz");
+//             break;
+//         case 3:
+//             console.log("qoniqarli");
+//             break;
+//         case 4:
+//             console.log("Yaxshi");
+//             break;
+//         case 5:
+//             console.log("A'lo");
+//             break;
+//         default:
+//             console.log("Xato");
+//     }
+// }
+// num(5);
+
+//4)
+// var input = 12321;
+// var soat = parseInt(input/3600) ;
+// var mnut = input - (soat * 3600);
+// console.log(soat + " soat " + mnut + " minut");
+
+//5)
+//javascript 1995-yilda yaratilgan
+//2009-yil backendi ishlagan
